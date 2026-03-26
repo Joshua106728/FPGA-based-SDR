524206ca7f9d9758c78d338c12923a3a3f75aad9
Joshua106728 <jhwjhwstl03@gmail.com>
2026-03-26 02:32:41 -0400

    added fifo example

--- sv/create_project.tcl
+++ sv/create_project.tcl
@@ -1,9 +1,29 @@
 # Set up a new project
-create_project SDR ./ -part xc7a25tcsg325-2 -force
+create_project FPGA_SDR ./ -part xc7a25tcsg325-2 -force
 
 set_property SIMULATOR_LANGUAGE Verilog [current_project]
 
 # ADD IP BLOCKS HERE
+##################################################################
+# CREATE IP fifo
+##################################################################
+
+set fifo [create_ip -name fifo_generator -vendor xilinx.com -library ip -version 13.2 -module_name fifo]
+
+# User Parameters
+set_property -dict [list \
+  CONFIG.Fifo_Implementation {Independent_Clocks_Block_RAM} \
+  CONFIG.Input_Depth {16} \
+  CONFIG.Valid_Flag {true} \
+] [get_ips fifo]
+
+# Runtime Parameters
+set_property -dict { 
+  GENERATE_SYNTH_CHECKPOINT {1}
+} $fifo
+
+generate_target all [get_ips fifo]
+##################################################################
 
 add_files constraints.xdc
 set_property FILE_TYPE XDC [get_files constraints.xdc]
---
+++ sv/include/types.sv
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+`ifndef TYPES
+`define TYPES
+
+package types;
+    parameter DW = 32;
+
+endpackage
+
+`endif 
